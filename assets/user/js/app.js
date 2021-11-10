0adc20a338f338eea0f1d23a64d01a258558c8f1
Shanbora <chanbora@logilink.fr>
2021-11-10 10:36:30 +0100

    add js functions to user page

--- assets/user/js/app.js
+++ assets/user/js/app.js
@@ -1,2 +1,26 @@
 import '../css/app.scss';
 import '@commonComponents/functions/toastrOptions';
+
+const routes = require('@publicFolder/js/fos_js_routes.json');
+import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Menu } from '@dashboardComponents/Layout/Menu';
+import { Notifications } from "@dashboardComponents/Notifications";
+
+Routing.setRoutingData(routes);
+
+const menu = document.getElementById("menu");
+if(menu) {
+    render(
+        <Menu {...menu.dataset} />, menu
+    )
+}
+
+const notifications = document.getElementById("notifications");
+if(notifications){
+    render(
+        <Notifications {...notifications.dataset} />, notifications
+    )
+}
