cecfd28cab86d2dbb1ecba5318729f1f526e46cb
Shanbora <chanbora@logilink.fr>
2021-11-10 10:15:47 +0100

    create common components fodler with toastr options

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -1,4 +1,5 @@
 import '../css/app.scss';
+import '@commonComponents/functions/toastrOptions';
 
 const routes = require('@publicFolder/js/fos_js_routes.json');
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
@@ -10,23 +11,6 @@ import { render } from 'react-dom';
 import { Menu }     from "@dashboardComponents/Layout/Menu";
 import { Cookies, CookiesGlobalResponse } from "@appComponents/Tools/Cookies";
 
-toastr.options = {
-    "closeButton": true,
-    "debug": false,
-    "newestOnTop": true,
-    "progressBar": true,
-    "positionClass": "toast-top-right",
-    "preventDuplicates": true,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-}
 Routing.setRoutingData(routes);
 
 const menu = document.getElementById("menu");
