c34e8d6de17f380653e982f4af6e5edf21ba1cd5
Shanbora <chanbora@logilink.fr>
2021-12-13 09:39:53 +0100

    semantic

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,12 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/espace-membre", name=""user_")
+ * @Route("/espace-membre", name="user_")
  */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/", options={"expose"=true}, name=homepage")
+     * @Route("/", options={"expose"=true}, name="homepage")
      */
     public function index(): Response
     {
