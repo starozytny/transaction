e3189c302fb27554be629e5affe4e0e5125eec17
Shanbora <chanbora@logilink.fr>
2021-12-13 09:37:08 +0100

    route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/espace-membre", name=""user_")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/espace-membre", options={"expose"=true}, name="user_homepage")
+     * @Route("/", options={"expose"=true}, name=homepage")
      */
     public function index(): Response
     {
