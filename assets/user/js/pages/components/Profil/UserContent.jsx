b7685d894638c19cb2eceb24f905319bb7b39592
Shanbora <chanbora@logilink.fr>
2021-12-17 10:02:27 +0100

    tab change context

--- assets/user/js/pages/components/Profil/UserContent.jsx
+++ assets/user/js/pages/components/Profil/UserContent.jsx
@@ -10,6 +10,12 @@ export class UserContent extends Component {
             context: "users",
             users: props.users,
         }
+
+        this.handleChangeContext = this.handleChangeContext.bind(this);
+    }
+
+    handleChangeContext = (context) => {
+        this.setState({ context })
     }
 
     render () {
@@ -18,18 +24,33 @@ export class UserContent extends Component {
 
         let content;
         switch (context){
+            case "negotiators":
+                content = <div>Hello négociateurs</div>
+                break;
+            case "agency":
+                content = <div>Hello Agency</div>
+                break;
             default:
                 content = <div id="profil-users"><Users donnees={users} id={id} isUser={isUser} /></div>
                 break;
         }
 
+        let tabs = [
+            { value: 'users', label: "Utilisateurs" },
+            { value: 'negotiators', label: "Négociateurs" },
+            { value: 'agency', label: "Agence" },
+        ]
+
         return <div className="page-default">
             <div>
                 <div className="title-col-2">
                     <div className="tab-col-2">
-                        <div className="item active">Utilisateurs</div>
-                        <div className="item">Négociateurs</div>
-                        <div className="item">Agence</div>
+                        {tabs.map(el => {
+                            return <div className={"item" + (el.value === context ? " active" : "")} key={el.value}
+                                        onClick={() => this.handleChangeContext(el.value)}>
+                                {el.label}
+                            </div>
+                        })}
                     </div>
                 </div>
             </div>
