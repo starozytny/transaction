f82c356ca1fc7350bdbbc840e9b0c34b4dbda29d
Shanbora <chanbora@logilink.fr>
2021-12-22 16:52:44 +0100

    interactive choice form financial

--- assets/user/js/pages/components/Biens/Steps/Step5Vente.jsx
+++ assets/user/js/pages/components/Biens/Steps/Step5Vente.jsx
@@ -9,12 +9,11 @@ import helper from "@userPages/components/Biens/helper";
 import Sanitaze from "@commonComponents/functions/sanitaze";
 
 export function Step5Vente({ step, onChange, onChangeSelect, onNext, errors,
-                      typeCalcul, price, provisionCharges, provisionOrdures, tva, totalTerme, caution, honoraireTtc,
-                          honoraireBail, edl, typeCharges, totalGeneral, typeBail, durationBail })
+                        price, chargesMensuelles, notaire, foncier, taxeHabitation, totalTerme, honoraireChargeDe,
+                        honorairePourcentage, honoraireTtc, totalGeneral, prixHorsAcquereur,
+                        isCopro, nbLot, chargesLot, isSyndicProcedure, detailsProcedure })
 {
-    let calculItems = helper.getItems("calculs")
-    let chargesItems = helper.getItems("charges")
-    let bailsItems = helper.getItems("bails")
+    let honoraireItems = helper.getItems("honoraires");
 
     return <div className={"step-section" + (step === 5 ? " active" : "")}>
         <div className="line-infos">
@@ -28,12 +27,12 @@ export function Step5Vente({ step, onChange, onChangeSelect, onNext, errors,
                 <Input type="number" step="any" identifiant="price" valeur={price} errors={errors} onChange={onChange}>
                     <span>Prix *</span>
                 </Input>
-                <Input type="number" step="any" identifiant="provisionCharges" valeur={provisionCharges} errors={errors} onChange={onChange}>
+                <Input type="number" step="any" identifiant="chargesMensuelles" valeur={chargesMensuelles} errors={errors} onChange={onChange}>
                     <span>Charges mensuelles</span>
                 </Input>
             </div>
             <div className="line line-2">
-                <Input type="number" step="any" identifiant="provisionCharges" valeur={provisionCharges} errors={errors} onChange={onChange}>
+                <Input type="number" step="any" identifiant="notaire" valeur={notaire} errors={errors} onChange={onChange}>
                     <span>Frais notaire</span>
                 </Input>
                 <div className="form-group">
@@ -42,10 +41,10 @@ export function Step5Vente({ step, onChange, onChangeSelect, onNext, errors,
                 </div>
             </div>
             <div className="line line-2">
-                <Input type="number" step="any" identifiant="provisionCharges" valeur={provisionCharges} errors={errors} onChange={onChange}>
+                <Input type="number" step="any" identifiant="foncier" valeur={foncier} errors={errors} onChange={onChange}>
                     <span>Impôt foncier</span>
                 </Input>
-                <Input type="number" step="any" identifiant="provisionOrdures" valeur={provisionOrdures} errors={errors} onChange={onChange}>
+                <Input type="number" step="any" identifiant="taxeHabitation" valeur={taxeHabitation} errors={errors} onChange={onChange}>
                     <span>Taxe habitation</span>
                 </Input>
             </div>
@@ -53,11 +52,11 @@ export function Step5Vente({ step, onChange, onChangeSelect, onNext, errors,
 
         <div className="line special-line">
             <div className="line line-3">
-                <SelectReactSelectize items={chargesItems} identifiant="typeCharges" valeur={typeCharges} errors={errors}
-                                      onChange={(e) => onChangeSelect('typeCharges', e)}>
-                    Honoraires à la charge
+                <SelectReactSelectize items={honoraireItems} identifiant="honoraireChargeDe" valeur={honoraireChargeDe} errors={errors}
+                                      onChange={(e) => onChangeSelect('honoraireChargeDe', e)}>
+                    Honoraires à la charge *
                 </SelectReactSelectize>
-                <Input type="number" step="any" identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
+                <Input type="number" step="any" identifiant="honorairePourcentage" valeur={honorairePourcentage} errors={errors} onChange={onChange}>
                     <span>Pourcentage des honoraires *</span>
                 </Input>
                 <Input type="number" step="any" identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
@@ -72,27 +71,39 @@ export function Step5Vente({ step, onChange, onChangeSelect, onNext, errors,
                     <div>{Sanitaze.toFormatCurrency(totalGeneral)}</div>
                 </div>
             </div>
+            {(honoraireChargeDe === 0 || honoraireChargeDe === 2) && <div className="line line-3">
+                <Input type="number" step="any" identifiant="prixHorsAcquereur" valeur={prixHorsAcquereur} errors={errors} onChange={onChange}>
+                    <span>Prix hors honoraire acquéreur</span>
+                </Input>
+                <div className="form-group" />
+                <div className="form-group" />
+            </div>}
         </div>
 
         <div className="line special-line">
             <div className="line line-3">
-                <Radiobox items={helper.getItems("answers-simple", 3)} identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
+                <Radiobox items={helper.getItems("answers-simple", 3)} identifiant="isCopro" valeur={isCopro} errors={errors} onChange={onChange}>
                     Bien en copropriété ?
                 </Radiobox>
-                <Input type="number" step="any" identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
-                    <span>Nombre de lots</span>
-                </Input>
-                <Input type="number" step="any" identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
-                    <span>Montant annuel des charges du lot</span>
-                </Input>
+                {parseInt(isCopro) === 1 ? <>
+                    <Input type="number" step="any" identifiant="nbLot" valeur={nbLot} errors={errors} onChange={onChange}>
+                        <span>Nombre de lots</span>
+                    </Input>
+                    <Input type="number" step="any" identifiant="chargesLot" valeur={chargesLot} errors={errors} onChange={onChange}>
+                        <span>Montant annuel des charges du lot</span>
+                    </Input>
+                </> : <>
+                    <div className="form-group" />
+                    <div className="form-group" />
+                </>}
             </div>
             <div className="line line-2">
-                <Radiobox items={helper.getItems("answers-simple", 3)} identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
-                    Le syndicat fait l'objet d'un plan de sauvegarde ?
+                <Radiobox items={helper.getItems("answers-simple", 4)} identifiant="isSyndicProcedure" valeur={isSyndicProcedure} errors={errors} onChange={onChange}>
+                    Le syndicat fait l'objet d'une procédure ?
                 </Radiobox>
-                <TextArea identifiant="honoraireTtc" valeur={honoraireTtc} errors={errors} onChange={onChange}>
+                {parseInt(isSyndicProcedure) === 1 ? <TextArea identifiant="detailsProcedure" valeur={detailsProcedure} errors={errors} onChange={onChange}>
                     <span>Détails de la procédure en cours</span>
-                </TextArea>
+                </TextArea> : <div className="form-group" />}
             </div>
         </div>
 
