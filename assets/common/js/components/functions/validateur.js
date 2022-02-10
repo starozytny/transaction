328e60f323101c029f79c20f8c9e0423bd1792e8
Shanbora <chanbora@logilink.fr>
2022-02-10 16:05:51 +0100

    mes

--- assets/common/js/components/functions/validateur.js
+++ assets/common/js/components/functions/validateur.js
@@ -1,3 +1,5 @@
+const Sanitaze = require('@commonComponents/functions/sanitaze');
+
 function validateDate($value) {
     if($value === "" || $value === null){
         return {
@@ -137,16 +139,18 @@ function validateDateLimitMinutes($value, $min, $max) {
 }
 
 function validateDateLimitHoursMinutes($value, $minH, $maxH, $minM, $maxM) {
+    let $msg = 'L\'horaire doit être compris entre ' + $minH + 'h' + $minM +'min et ' + $maxH + 'h' + $maxM + 'min.';
+
     if($value.getHours() < $minH || $value.getHours() > $maxH){
         return {
             'code': false,
-            'message': 'L\'horaire doit être compris entre ' + $minH + 'h' + $minM +'min et ' + $maxH + 'h' + $maxM + 'min.'
+            'message': '[H]' + $msg
         };
     }else{
         if($value.getMinutes() < $minM || $value.getMinutes() > $maxM){
             return {
                 'code': false,
-                'message': 'L\'horaire doit être compris entre ' + $minH + 'h' + $minM +'min et ' + $maxH + 'h' + $maxM + 'min.'
+                'message': '[M]' + $msg
             };
         }
     }
