628b21a086052454f14a0e6e1708ee06179b419a
Shanbora <chanbora@logilink.fr>
2022-02-11 10:57:51 +0100

    add action function for default action send mail and agenda

--- assets/dashboard/js/pages/components/Immo/Owners/OwnersItem.jsx
+++ assets/dashboard/js/pages/components/Immo/Owners/OwnersItem.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 import Routing          from '@publicFolder/bundles/fosjsrouting/js/router.min.js';
 
-import { ButtonIcon, ButtonIconDropdown, LinkContact } from "@dashboardComponents/Tools/Button";
+import Actions from "@userComponents/functions/actions";
+
+import { ButtonIcon, ButtonIconDropdown } from "@dashboardComponents/Tools/Button";
 import { Selector }  from "@dashboardComponents/Layout/Selector";
 import { UtContact } from "@dashboardComponents/Tools/Utilitaire";
 
@@ -21,11 +23,6 @@ export class OwnersItem extends Component {
                 }
             })
         }
-
-        let actions = [
-            {data: <LinkContact isClient={isClient} email={elem.email} />},
-            {data: <a href={Routing.generate('user_agenda', {'ty': 'owner', 'se': elem.id})}>Voir ses rendez-vous</a>},
-        ]
 
         return <div className="item">
             {!isClient && <Selector id={elem.id} onSelectors={onSelectors} />}
@@ -58,7 +55,7 @@ export class OwnersItem extends Component {
                                 <ButtonIcon icon="pencil" onClick={() => onChangeContext("update", elem)}>Modifier</ButtonIcon>
                                 {!isFormBien && <ButtonIcon icon="trash" onClick={() => onDelete(elem)}>Supprimer</ButtonIcon>}
                             </>}
-                            <ButtonIconDropdown icon="dropdown" items={actions}>Autres</ButtonIconDropdown>
+                            <ButtonIconDropdown icon="dropdown" items={Actions.getDefaultAction(isClient, elem)}>Autres</ButtonIconDropdown>
                         </div>}
                     </div>
                 </div>
---
+++ assets/user/js/components/functions/actions.js
@@ -0,0 +1,16 @@
+const React = require("react");
+
+const Routing = require('@publicFolder/bundles/fosjsrouting/js/router.min.js');
+
+const { LinkContact } = require("@dashboardComponents/Tools/Button");
+
+function getDefaultAction(isClient, elem) {
+    return [
+        {data: <LinkContact isClient={isClient} email={elem.email} />},
+        {data: <a href={Routing.generate('user_agenda', {'ty': 'owner', 'se': elem.id})}>Voir ses rendez-vous</a>},
+    ]
+}
+
+module.exports = {
+    getDefaultAction
+}
