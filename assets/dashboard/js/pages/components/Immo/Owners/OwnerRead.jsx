99cbaa9c6aeda7b7223664ec4eaa536cbe5f8a3d
Shanbora <chanbora@logilink.fr>
2022-02-17 17:12:18 +0100

    reader owner

--- assets/dashboard/js/pages/components/Immo/Owners/OwnerRead.jsx
+++ assets/dashboard/js/pages/components/Immo/Owners/OwnerRead.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import { ButtonIcon }   from "@dashboardComponents/Tools/Button";
 import { Back }         from "@dashboardComponents/Layout/Elements";
 import { AdCard }       from "@userPages/components/Biens/AdCard";
 import { Alert }        from "@dashboardComponents/Tools/Alert";
+import { ReadCard }     from "@userComponents/Layout/Read";
 
 export class OwnerRead extends Component {
     render () {
@@ -24,55 +24,12 @@ export class OwnerRead extends Component {
             <div>
                 <Back onChangeContext={onChangeContext} />
 
-                <div className="item-read">
+                <ReadCard elem={elem} onChangeContext={onChangeContext} totalBiens={totalBiens} />
 
-                    <div className="item-read-infos item-read-infos-without-img">
-                        {!elem.isGerance && <div className="actions">
-                            <ButtonIcon icon="pencil" onClick={() => onChangeContext('update', elem)} >Modifier</ButtonIcon>
-                        </div>}
-
-                        <div className="item-read-infos-container">
-
-                            <div className="main-infos">
-                                <div className="name">
-                                    <div>#{elem.code}</div>
-                                    <span>{elem.fullname}</span>
-                                </div>
-                                <div className="username">
-                                    <span>({elem.agency.name})</span>
-                                </div>
-                                <div className="sub">{elem.email}</div>
-                                <div className="sub">{elem.phone1}</div>
-                                <div className="sub">{elem.phone2}</div>
-                                <div className="username">
-                                    <span>{elem.phone3}</span>
-                                </div>
-                                <div className="username">
-                                    <span>{elem.fullAddress}</span>
-                                </div>
-
-                                {elem.negotiator && <div className="sub">Négociateur : {elem.negotiator.fullname}</div>}
-
-                            </div>
-
-                            <div className="footer-infos">
-                                {elem.isGerance && <>
-                                    <div className="badge role-time">Gérance</div>
-                                    <div className="badge role-time">{elem.codeGerance}</div>
-                                    <div className="badge">{totalBiens} biens</div>
-                                </>}
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="item-read-content">
-                        <div className="content">
-                            {itemsBien.length !== 0 ? itemsBien.map(el => {
-                                return <AdCard el={el} isOwnerPage={true} key={el.id}/>
-                            }) : <Alert>Aucun bien</Alert>}
-                        </div>
-                    </div>
-
+                <div className="item-content-2">
+                    {itemsBien.length !== 0 ? itemsBien.map(el => {
+                        return <AdCard el={el} isOwnerPage={true} key={el.id}/>
+                    }) : <Alert>Aucun bien</Alert>}
                 </div>
             </div>
         </>
--- assets/user/js/components/Layout/Read.jsx
+++ assets/user/js/components/Layout/Read.jsx
@@ -3,8 +3,10 @@ import React from "react";
 import { ButtonIcon }       from "@dashboardComponents/Tools/Button";
 import { NegotiatorBubble } from "@dashboardPages/components/Immo/Negociators/NegotiatorsItem";
 
-export function ReadCard ({ displayActions = true, elem, avatar, onChangeContext })
+export function ReadCard (props)
 {
+    const { displayActions = true, elem, avatar, onChangeContext, totalBiens = null } = props;
+
     return <div className="item-read-2-container">
         <div className="item-read-2">
             <div className="col-1">
@@ -14,10 +16,12 @@ export function ReadCard ({ displayActions = true, elem, avatar, onChangeContext
                 <div className="infos">
                     <div className="name">
                         <span>{elem.fullname}</span>
-                        {displayActions && <div className="actions">
+                        {(displayActions && !elem.isGerance) && <div className="actions">
                             <ButtonIcon icon="pencil" onClick={() => onChangeContext('update', elem)} text="Modifier" />
                         </div>}
                     </div>
+                    {elem.agency && <div className="sub-icon">{elem.agency.name}</div>}
+                    {elem.code && <div className="badge badge-default">#{elem.code}</div>}
                     {elem.isArchived && <div className="badge badge-warning">Archivé</div>}
                     {elem.lastContactAtAgo && <div className="sub">
                         Dernier contact : {elem.lastContactAtAgo}
@@ -36,6 +40,11 @@ export function ReadCard ({ displayActions = true, elem, avatar, onChangeContext
 
                 <div className="item">
                     <div className="badges">
+                        {elem.isGerance && <>
+                            <div className="badge badge-default">Gérance</div>
+                            <div className="badge badge-default">{elem.codeGerance}</div>
+                            {totalBiens !== null && <div className="badge">{totalBiens} bien{totalBiens > 1 ? "s" : ""}</div>}
+                        </>}
                         {elem.status && <div className={"badge badge-" + elem.status}>{elem.statusString}</div>}
                         {elem.type && <div className="badge badge-default">Type de prospect : {elem.typeString}</div>}
                     </div>
