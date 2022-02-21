5c485801dbb3e0ab82b9924d3e7e9fadb09e5cd5
Shanbora <chanbora@logilink.fr>
2022-02-21 11:40:35 +0100

    form support from user

--- assets/user/js/pages/components/Support/SupportForm.jsx
+++ assets/user/js/pages/components/Support/SupportForm.jsx
@@ -4,74 +4,37 @@ import axios                   from "axios";
 import toastr                  from "toastr";
 import Routing                 from '@publicFolder/bundles/fosjsrouting/js/router.min.js';
 
-import { Input, Radiobox, Select } from "@dashboardComponents/Tools/Fields";
+import { Input }               from "@dashboardComponents/Tools/Fields";
 import { Alert }               from "@dashboardComponents/Tools/Alert";
 import { Button }              from "@dashboardComponents/Tools/Button";
 import { FormLayout }          from "@dashboardComponents/Layout/Elements";
 
 import Validateur              from "@commonComponents/functions/validateur";
 import Helper                  from "@commonComponents/functions/helper";
-import NegotiatorFunction      from "@commonComponents/functions/negotiator";
 import Formulaire              from "@dashboardComponents/functions/Formulaire";
 
-import { SelecteurNegociateur } from "@dashboardPages/components/Immo/Elements/Selecteur";
-import { LocalisationContact }  from "@dashboardPages/components/Immo/Elements/Contact";
-
-const URL_CREATE_ELEMENT     = "api_owners_create";
 const URL_UPDATE_GROUP       = "api_owners_update";
-const TXT_CREATE_BUTTON_FORM = "Enregistrer";
 const TXT_UPDATE_BUTTON_FORM = "Enregistrer les modifications";
 
-export function SupportFormulaire ({ type, onChangeContext, onUpdateList, element, isClient = false,
-                                     societies, societyId = "", agencies, agencyId = "",
-                                     negotiators })
+export function SupportFormulaire ({ type, onChangeContext, onUpdateList, element })
 {
-    let title = "Ajouter un propriétaire";
-    let url = Routing.generate(URL_CREATE_ELEMENT);
-    let msg = "Félicitations ! Vous avez ajouté un nouveau propriétaire !"
-
-    if(type === "update"){
-        title = "Modifier " + element.lastname + " " + element.firstname;
-        url = Routing.generate(URL_UPDATE_GROUP, {'id': element.id});
-        msg = "Félicitations ! La mise à jour s'est réalisée avec succès !";
-    }
+    let title = "Modifier " + element.name;
+    let url = Routing.generate(URL_UPDATE_GROUP, {'id': element.id});
+    let msg = "Félicitations ! La mise à jour s'est réalisée avec succès !";
 
     let form = <SupportForm
         context={type}
         url={url}
-        society={element ? element.society.id : societyId}
-        agency={element ? element.agency.id : agencyId}
-        negotiator={element ? (element.negotiator ? element.negotiator.id : "") : ""}
-        lastname={element ? Formulaire.setValueEmptyIfNull(element.lastname) : ""}
-        firstname={element ? Formulaire.setValueEmptyIfNull(element.firstname) : ""}
-        civility={element ? Formulaire.setValueEmptyIfNull(element.civility, 2) : 2}
-        phone1={element ? Formulaire.setValueEmptyIfNull(element.phone1) : ""}
-        phone2={element ? Formulaire.setValueEmptyIfNull(element.phone2) : ""}
-        phone3={element ? Formulaire.setValueEmptyIfNull(element.phone3) : ""}
-        email={element ? Formulaire.setValueEmptyIfNull(element.email) : ""}
-        address={element ? Formulaire.setValueEmptyIfNull(element.address) : ""}
-        complement={element ? Formulaire.setValueEmptyIfNull(element.complement) : ""}
-        zipcode={element ? Formulaire.setValueEmptyIfNull(element.zipcode) : ""}
-        city={element ? Formulaire.setValueEmptyIfNull(element.city) : ""}
-        country={element ? Formulaire.setValueEmptyIfNull(element.country, "France") : "France"}
-        category={element ? Formulaire.setValueEmptyIfNull(element.category, "") : ""}
 
-        isCoIndivisaire={element ? (element.isCoIndivisaire ? 1 : 0) : 0}
-        coLastname={element ? Formulaire.setValueEmptyIfNull(element.coLastname) : ""}
-        coFirstname={element ? Formulaire.setValueEmptyIfNull(element.coFirstname) : ""}
-        coPhone={element ? Formulaire.setValueEmptyIfNull(element.coPhone) : ""}
-        coEmail={element ? Formulaire.setValueEmptyIfNull(element.coEmail) : ""}
-        coAddress={element ? Formulaire.setValueEmptyIfNull(element.coAddress) : ""}
-        coZipcode={element ? Formulaire.setValueEmptyIfNull(element.coZipcode) : ""}
-        coCity={element ? Formulaire.setValueEmptyIfNull(element.coCity) : ""}
+        filename={Formulaire.setValueEmptyIfNull(element.filename)}
+        ftpServer={Formulaire.setValueEmptyIfNull(element.ftpServer)}
+        ftpPort={Formulaire.setValueEmptyIfNull(element.ftpPort)}
+        ftpUser={Formulaire.setValueEmptyIfNull(element.ftpUser)}
+        ftpPassword={Formulaire.setValueEmptyIfNull(element.ftpPassword)}
 
         onUpdateList={onUpdateList}
         onChangeContext={onChangeContext}
         messageSuccess={msg}
-        societies={societies}
-        agencies={agencies}
-        negotiators={negotiators}
-        isClient={isClient}
     />
 
     return <FormLayout onChangeContext={onChangeContext} form={form}>{title}</FormLayout>
@@ -83,70 +46,40 @@ export class SupportForm extends Component {
         super(props);
 
         this.state = {
-            society: props.society,
-            agency: props.agency,
-            negotiator: props.negotiator,
-            lastname: props.lastname,
-            firstname: props.firstname,
-            civility: props.civility ,
-            phone1: props.phone1,
-            phone2: props.phone2,
-            phone3: props.phone3,
-            email: props.email,
-            address: props.address,
-            complement: props.complement,
-            zipcode: props.zipcode,
-            city: props.city,
-            country: props.country,
-            category: props.category,
-
-            isCoIndivisaire: props.isCoIndivisaire,
-            coLastname: props.lastname,
-            coFirstname: props.firstname,
-            coPhone: props.phone1,
-            coEmail: props.email,
-            coAddress: props.address,
-            coZipcode: props.zipcode,
-            coCity: props.city,
-
+            filename: props.filename,
+            ftpServer: props.ftpServer,
+            ftpPort: props.ftpPort,
+            ftpUser: props.ftpUser,
+            ftpPassword: props.ftpPassword,
             errors: [],
             success: false,
             critere: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleChangeSelect = this.handleChangeSelect.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
         Helper.toTop();
-        document.getElementById("lastname").focus()
+        document.getElementById("filename").focus()
     }
 
     handleChange = (e) => { this.setState({[e.currentTarget.name]: e.currentTarget.value}) }
 
-    handleChangeSelect = (name, e) => {
-        NegotiatorFunction.changeSelectNegotiator(this, this.state.negotiator, name, e);
-    }
-
     handleSubmit = (e) => {
         e.preventDefault();
 
-        const { context, url, messageSuccess } = this.props;
-        const { critere, society, lastname, phone1, phone2, phone3, email } = this.state;
+        const { url, messageSuccess } = this.props;
+        const { critere, filename } = this.state;
 
         if(critere !== ""){
             toastr.error("Veuillez rafraichir la page.");
         }else{
-            this.setState({ success: false});
+            this.setState({ errors: [], success: false });
 
             let paramsToValidate = [
-                {type: "text",       id: 'society',   value: society},
-                {type: "text",       id: 'lastname',  value: lastname},
-                {type: "text",       id: 'email',     value: email},
-                {type: "atLeastOne", id: 'phone1',    value: phone1, idCheck: 'phone2', valueCheck: phone2},
-                {type: "atLeastOne", id: 'phone1',    value: phone1, idCheck: 'phone3', valueCheck: phone3},
+                {type: "text", id: 'filename', value: filename},
             ];
 
             // validate global
@@ -157,10 +90,7 @@ export class SupportForm extends Component {
                 Formulaire.loader(true);
                 let self = this;
 
-                let formData = new FormData();
-                formData.append("data", JSON.stringify(this.state));
-
-                axios({ method: "POST", url: url, data: formData, headers: {'Content-Type': 'multipart/form-data'} })
+                axios({ method: "PUT", url: url, data: this.state })
                     .then(function (response) {
                         let data = response.data;
                         Helper.toTop();
@@ -170,32 +100,6 @@ export class SupportForm extends Component {
                         }
 
                         self.setState({ success: messageSuccess, errors: [] });
-                        if(context === "create"){
-                            toastr.info(messageSuccess);
-                            self.setState( {
-                                lastname: "",
-                                firstname: "",
-                                civility: 2,
-                                phone1: "",
-                                phone2: "",
-                                phone3: "",
-                                email: "",
-                                address: "",
-                                complement: "",
-                                zipcode: "",
-                                city: "",
-                                country: "",
-                                category: "",
-                                isCoIndivisaire: 0,
-                                coLastname: "",
-                                coFirstname: "",
-                                coPhone: "",
-                                coEmail: "",
-                                coAddress: "",
-                                coZipcode: "",
-                                coCity: "",
-                            })
-                        }
                     })
                     .catch(function (error) {
                         Formulaire.displayErrors(self, error);
@@ -209,27 +113,7 @@ export class SupportForm extends Component {
     }
 
     render () {
-        const { context } = this.props;
-        const { critere, errors, success, society, agency, negotiator, lastname, firstname, civility, category,
-            isCoIndivisaire, coLastname, coFirstname, coPhone, coEmail, coAddress, coZipcode, coCity } = this.state;
-
-        let coindivisaireItems = [
-            {value: 1, label: "Oui", identifiant: "oui"},
-            {value: 0, label: "Non", identifiant: "no"},
-        ]
-
-        let civilityItems = [
-            {value: 0, label: "Mr",         identifiant: "mr"},
-            {value: 1, label: "Mme",        identifiant: "mme"},
-            {value: 2, label: "Société",    identifiant: "societe"}
-        ]
-
-        let selectCategory = [
-            {value: 0, label: "Habitation",             identifiant: "habitation"},
-            {value: 1, label: "Des murs",               identifiant: "des-murs"},
-            {value: 2, label: "Du fond de commerce",    identifiant: "commerce"},
-            {value: 3, label: "Location",               identifiant: "location"},
-        ]
+        const { critere, errors, success, filename, ftpServer, ftpPort, ftpUser, ftpPassword } = this.state;
 
         return <>
             <form onSubmit={this.handleSubmit}>
@@ -243,81 +127,43 @@ export class SupportForm extends Component {
                         </div>
 
                         <div className="line">
-                            <Radiobox items={civilityItems} identifiant="civility" valeur={civility} errors={errors} onChange={this.handleChange}>
-                                Civilité
-                            </Radiobox>
-                        </div>
-
-                        <div className="line line-2">
-                            <Input valeur={lastname} identifiant="lastname" errors={errors} onChange={this.handleChange}>
-                                {parseInt(civility) !== 2 ? "Nom *" : "Raison sociale *"}
+                            <Input valeur={filename} identifiant="filename" errors={errors} onChange={this.handleChange}>
+                                Nom du fichier
                             </Input>
-                            {parseInt(civility) !== 2 ? <Input valeur={firstname} identifiant="firstname" errors={errors} onChange={this.handleChange}>Prénom</Input>
-                                : <div className="form-group" />}
-                        </div>
-
-                        <div className="line">
-                            <Select items={selectCategory} identifiant="category" valeur={category} errors={errors} onChange={this.handleChange}>
-                                Catégorie de propriétaire
-                            </Select>
                         </div>
                     </div>
                     <div className="form-group">
                         <div className="line-separator">
-                            <div className="title">Négociateur</div>
+                            <div className="title">Serveur FTP</div>
                         </div>
 
-                        <SelecteurNegociateur {...this.props} onChangeSelect={this.handleChangeSelect} errors={errors}
-                                              society={society} agency={agency} negotiator={negotiator}/>
+                        <div className="line line-2">
+                            <Input valeur={ftpServer} identifiant="ftpServer" errors={errors} onChange={this.handleChange}>
+                                Nom du serveur
+                            </Input>
+                            <Input valeur={ftpPort} identifiant="ftpPort" errors={errors} onChange={this.handleChange}>
+                                Port
+                            </Input>
+                        </div>
 
+                        <div className="line line-2">
+                            <Input valeur={ftpUser} identifiant="ftpUser" errors={errors} onChange={this.handleChange}>
+                                Identifiant
+                            </Input>
+                            <Input valeur={ftpPassword} identifiant="ftpPassword" errors={errors} onChange={this.handleChange}>
+                                Mot de passe
+                            </Input>
+                        </div>
                     </div>
-                </div>
-
-                <div className="line line-2">
-                    <LocalisationContact {...this.state} onChange={this.handleChange} />
                 </div>
 
                 <div className="line line-critere">
                     <Input identifiant="critere" valeur={critere} errors={errors} onChange={this.handleChange}>Critère</Input>
                 </div>
 
-                <div className="line-separator">
-                    <div className="title">Co-indivisaire</div>
-                </div>
-
-                <div className="line">
-                    <Radiobox items={coindivisaireItems} identifiant="isCoIndivisaire" valeur={isCoIndivisaire} errors={errors} onChange={this.handleChange}>
-                        Co-indivisaire ?
-                    </Radiobox>
-                </div>
-
-                {parseInt(isCoIndivisaire) === 1 && <>
-                    <div className="line line-2">
-                        <div className="form-group">
-                            <div className="line line-2">
-                                <Input valeur={coLastname} identifiant="coLastname" errors={errors} onChange={this.handleChange}>Nom</Input>
-                                <Input valeur={coFirstname} identifiant="coFirstname" errors={errors} onChange={this.handleChange}>Prénom</Input>
-                            </div>
-
-                            <div className="line line-2">
-                                <Input valeur={coPhone} identifiant="coPhone" errors={errors} onChange={this.handleChange}>Téléphone</Input>
-                                <Input valeur={coEmail} identifiant="coEmail" errors={errors} onChange={this.handleChange} type="email" >Adresse e-mail</Input>
-                            </div>
-
-                            <div className="line line-3">
-                                <Input valeur={coAddress} identifiant="coAddress" errors={errors} onChange={this.handleChange}>Adresse</Input>
-                                <Input valeur={coZipcode} identifiant="coZipcode" errors={errors} onChange={this.handleChange}>Code postal</Input>
-                                <Input valeur={coCity} identifiant="coCity" errors={errors} onChange={this.handleChange}>Ville</Input>
-                            </div>
-                        </div>
-                        <div className="form-group" />
-                    </div>
-
-                </>}
-
                 <div className="line line-buttons">
                     <div className="form-button">
-                        <Button isSubmit={true}>{context === "create" ? TXT_CREATE_BUTTON_FORM : TXT_UPDATE_BUTTON_FORM}</Button>
+                        <Button isSubmit={true}>{TXT_UPDATE_BUTTON_FORM}</Button>
                     </div>
                 </div>
             </form>
--- assets/user/js/pages/components/Support/SupportsItem.jsx
+++ assets/user/js/pages/components/Support/SupportsItem.jsx
@@ -11,13 +11,14 @@ export class SupportsItem extends Component {
                 <div className="item-body">
                     <div className="infos infos-col-3">
                         <div className="col-1">
+                            <div className="badge badge-default">#00{elem.code}</div>
                             <div className="name">{elem.name}</div>
-                            <div className="badge badge-default">#{elem.code}</div>
                             <div className={"sub" + (!elem.filename ? " txt-danger" : "")}>Nom du fichier : {elem.filename ? elem.filename : "A définir"}</div>
                         </div>
 
                         <div className="col-2">
                             <div className="sub">{elem.ftpServer}</div>
+                            <div className="sub">Port : {elem.ftpPort}</div>
                             <div className="sub">{elem.ftpUser}</div>
                             <div className="sub">{elem.ftpPassword}</div>
                             <div className="sub">{elem.maxPhotos} photos max</div>
