12e6a469ce0ac259ba38f190d18ebd1fd2f78c83
Shanbora <chanbora@logilink.fr>
2022-02-23 10:25:14 +0100

    better btn status biens

--- assets/user/js/pages/components/Biens/BiensList.jsx
+++ assets/user/js/pages/components/Biens/BiensList.jsx
@@ -49,11 +49,11 @@ export class BiensList extends Component {
                 <div className="page-col-2">
                     <div className="title-col-2">
                         <div className="tab-col-2">
-                            <div className={"item" + (pageStatus === false ? " active" : "")}><a href={Routing.generate('user_biens')}>Tous</a></div>
-                            <div className={"item" + (pageStatus === 1 ? " active" : "")}><a href={Routing.generate('user_biens', {'st': 1})}>Actif</a></div>
-                            <div className={"item" + (pageStatus === 0 ? " active" : "")}><a href={Routing.generate('user_biens', {'st': 0})}>Inactif</a></div>
-                            <div className={"item" + (pageStatus === 3 ? " active" : "")}><a href={Routing.generate('user_biens', {'st': 3})}>Brouillon</a></div>
-                            <div className={"item" + (pageStatus === 2 ? " active" : "")}><a href={Routing.generate('user_biens', {'st': 2})}>Archive</a></div>
+                            <a href={Routing.generate('user_biens')}            className={"item" + (pageStatus === false ? " active" : "")}>Tous</a>
+                            <a href={Routing.generate('user_biens', {'st': 1})} className={"item" + (pageStatus === 1 ? " active" : "")}>Actif</a>
+                            <a href={Routing.generate('user_biens', {'st': 0})} className={"item" + (pageStatus === 0 ? " active" : "")}>Inactif</a>
+                            <a href={Routing.generate('user_biens', {'st': 3})} className={"item" + (pageStatus === 3 ? " active" : "")}>Brouillon</a>
+                            <a href={Routing.generate('user_biens', {'st': 2})} className={"item" + (pageStatus === 2 ? " active" : "")}>Archive</a>
                         </div>
                         <Button type="primary" element="a" onClick={Routing.generate('user_biens_create')}>Ajouter un bien</Button>
                     </div>
