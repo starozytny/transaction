217a0c6a50f70defe3b790e3f94b416e3c795897
Shanbora <chanbora@logilink.fr>
2022-02-25 09:11:43 +0100

    prepare history price entity

---
+++ src/Entity/History/HiPrice.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity\History;
+
+use App\Entity\DataEntity;
+use App\Repository\History\HiPriceRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=HiPriceRepository::class)
+ */
+class HiPrice extends DataEntity
+{
+    const HISTORY_PRICE = ["history:price"];
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"history:price"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Groups({"history:price"})
+     */
+    private $bienId;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Groups({"history:price"})
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = $this->initNewDate();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBienId(): ?int
+    {
+        return $this->bienId;
+    }
+
+    public function setBienId(int $bienId): self
+    {
+        $this->bienId = $bienId;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     * @Groups({"history:price"})
+     */
+    public function getCreatedAtJavascript(): ?string
+    {
+        return $this->setDateJavascript($this->createdAt);
+    }
+}
