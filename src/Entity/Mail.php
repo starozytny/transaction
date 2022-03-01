56ca95804e7b1242c2509ae3d75b6a78240f1950
Shanbora <chanbora@logilink.fr>
2022-03-01 16:23:36 +0100

    prepapre mail entity

---
+++ src/Entity/Mail.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MailRepository::class)
+ */
+class Mail extends DataEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $expeditor;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $destinators;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = $this->initNewDate();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getExpeditor(): ?string
+    {
+        return $this->expeditor;
+    }
+
+    public function setExpeditor(string $expeditor): self
+    {
+        $this->expeditor = $expeditor;
+
+        return $this;
+    }
+
+    public function getDestinators(): ?string
+    {
+        return $this->destinators;
+    }
+
+    public function setDestinators(string $destinators): self
+    {
+        $this->destinators = $destinators;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
