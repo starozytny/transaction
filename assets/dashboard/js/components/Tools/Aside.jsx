421a00ea99918a8b2014171287e26001cde11198
Shanbora <chanbora@logilink.fr>
2022-03-11 17:09:30 +0100

    aside

--- assets/dashboard/js/components/Tools/Aside.jsx
+++ assets/dashboard/js/components/Tools/Aside.jsx
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 
+function manageDoubleAside(isDoubleAside, isOpen){
+    if(isDoubleAside) {
+        let firstAside = document.querySelector(".main-content > .aside > .aside-content");
+        let secondOverlay = document.querySelector(".main-content > .aside > .aside-content .aside > .aside-overlay");
+        if (firstAside) {
+            firstAside.scrollTop = 0;
+            firstAside.style.overflow = isOpen ? "hidden" : "auto";
+            if(secondOverlay){
+                secondOverlay.style.backgroundColor = "rgba(24,24,24,0.7)"
+            }
+        }
+    }
+}
+
 export class Aside extends Component {
     constructor (props) {
         super(props)
 
         this.state = {
             title: null,
-            active: false
+            active: false,
+            isDoubleAside: props.isDoubleAside ? props.isDoubleAside : false
         }
 
         this.handleOpen = this.handleOpen.bind(this)
         this.handleClose = this.handleClose.bind(this)
     }
 
-    handleOpen = (title) => { this.setState({ active: true, title: title }) }
-    handleClose = () => { this.setState({ active: false }) }
+    handleOpen = (title) => {
+        manageDoubleAside(this.state.isDoubleAside, true);
+        let body = document.querySelector("body");
+        if(body){
+            body.style.overflow = "hidden";
+        }
+        this.setState({ active: true, title: title })
+    }
+    handleClose = () => {
+        manageDoubleAside(this.state.isDoubleAside, false);
+
+        let body = document.querySelector("body");
+        if(body){
+            body.style.overflow = "auto";
+        }
+        this.setState({ active: false })
+    }
 
     render () {
-        const { content, children } = this.props
+        const { content, children, classes } = this.props
         const { active, title } = this.state
 
-        return <div className={`aside ${active}`}>
+        return <div className={"aside " + active + (classes ? " " + classes : "") }>
             <div className="aside-overlay" onClick={this.handleClose} />
             <div className="aside-content">
                 <div className="aside-title">
