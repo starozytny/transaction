204ed8e30dd6cbff8733c9b66555a1edb728663b
Shanbora <chanbora@logilink.fr>
2022-04-06 11:57:03 +0200

    sort without accent and lowercase

--- assets/common/js/components/functions/sort.js
+++ assets/common/js/components/functions/sort.js
@@ -1,21 +1,23 @@
+const Sanitaze = require('@commonComponents/functions/sanitaze')
+
 function compareFirstname(a, b){
-    return comparison(a.firstname, b.firstname);
+    return compareWithoutAccent(a.firstname, b.firstname);
 }
 
 function compareLastname(a, b){
-    return comparison(a.lastname, b.lastname);
+    return compareWithoutAccent(a.lastname, b.lastname);
 }
 
 function compareUsername(a, b){
-    return comparison(a.username, b.username);
+    return compareWithoutAccent(a.username, b.username);
 }
 
 function compareTitle(a, b){
-    return comparison(a.title, b.title);
+    return compareWithoutAccent(a.title, b.title);
 }
 
 function compareName(a, b){
-    return comparison(a.name, b.name);
+    return compareWithoutAccent(a.name, b.name);
 }
 
 function compareCreatedAt(a, b){
@@ -27,15 +29,21 @@ function compareCreatedAtInverse(a, b){
 }
 
 function compareEmail(a, b){
-    return comparison(a.email, b.email);
+    return compareWithoutAccent(a.email, b.email);
 }
 
 function compareZipcode(a, b){
-    return comparison(a.zipcode, b.zipcode);
+    return compareWithoutAccent(a.zipcode, b.zipcode);
 }
 
 function compareCity(a,b){
-    return comparison(a.city, b.city);
+    return compareWithoutAccent(a.city, b.city);
+}
+
+function compareWithoutAccent(aVal, bVal) {
+    let aName = Sanitaze.removeAccents(aVal);
+    let bName = Sanitaze.removeAccents(bVal);
+    return comparison(aName.toLowerCase(), bName.toLowerCase());
 }
 
 function comparison (objA, objB){
