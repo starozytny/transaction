c1e12703285e183028dc2980bb36f5e16691c66b
Shanbora <chanbora@logilink.fr>
2022-06-02 10:18:48 +0200

    publish controler

---
+++ src/Controller/User/PublishController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Service\Immo\ImmoService;
+use App\Transaction\Entity\Immo\ImBien;
+use App\Transaction\Entity\Immo\ImPublish;
+use App\Transaction\Entity\Immo\ImStat;
+use App\Entity\User;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/espace-membre/publications", name="user_publications_")
+ */
+class PublishController extends AbstractController
+{
+    private $doctrine;
+    private $immoService;
+
+    public function __construct(ManagerRegistry $doctrine, ImmoService $immoService)
+    {
+        $this->doctrine = $doctrine;
+        $this->immoService = $immoService;
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function publications(SerializerInterface $serializer): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $em = $this->immoService->getEntityUserManager($user);
+
+        $data = $em->getRepository(ImBien::class)->findBy([
+            'agency' => $user->getAgencyId(), 'status' => ImBien::STATUS_ACTIF, 'isDraft' => false, 'isArchived' => false
+        ]);
+        $publishes = $em->getRepository(ImPublish::class)->findBy(['bien' => $data]);
+
+        $data = $serializer->serialize($data, 'json', ['groups' => User::USER_READ]);
+        $publishes = $serializer->serialize($publishes, 'json', ['groups' => ImPublish::PUBLISH_READ]);
+
+        return $this->render('user/pages/publications/index.html.twig', [
+            'donnees' => $data,
+            'publishes' => $publishes,
+        ]);
+    }
+
+    /**
+     * @Route("/historique", name="histories")
+     */
+    public function publicationsHistories(SerializerInterface $serializer): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $em = $this->immoService->getEntityUserManager($user);
+
+        $stats = $em->getRepository(ImStat::class)->findBy(['agency' => $user->getAgencyId()], ['createdAt' => 'DESC']);
+
+        $stats = $serializer->serialize($stats, 'json', ['groups' => ImStat::STAT_READ]);
+
+        return $this->render('user/pages/publications/history.html.twig', [
+            'donnees' => $stats,
+        ]);
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,19 +10,10 @@ use App\Entity\Mail;
 use App\Transaction\Entity\Donnee\DoQuartier;
 use App\Transaction\Entity\Donnee\DoSol;
 use App\Transaction\Entity\Donnee\DoSousType;
-use App\Transaction\Entity\History\HiVisite;
 use App\Transaction\Entity\Immo\ImAgency;
 use App\Transaction\Entity\Immo\ImBien;
-use App\Transaction\Entity\Immo\ImContract;
 use App\Transaction\Entity\Immo\ImNegotiator;
-use App\Transaction\Entity\Immo\ImOffer;
-use App\Transaction\Entity\Immo\ImOwner;
-use App\Transaction\Entity\Immo\ImPhoto;
-use App\Transaction\Entity\Immo\ImPublish;
-use App\Transaction\Entity\Immo\ImRoom;
-use App\Transaction\Entity\Immo\ImSettings;
 use App\Transaction\Entity\Immo\ImStat;
-use App\Transaction\Entity\Immo\ImSuivi;
 use App\Transaction\Entity\Immo\ImSupport;
 use App\Transaction\Entity\Immo\ImVisit;
 use App\Entity\User;
@@ -34,7 +25,6 @@ use App\Transaction\Repository\Immo\ImOwnerRepository;
 use App\Transaction\Repository\Immo\ImProspectRepository;
 use App\Transaction\Repository\Immo\ImTenantRepository;
 use App\Repository\UserRepository;
-use App\Service\Immo\SearchService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use App\Transaction\Repository\Agenda\AgEventRepository;
 use App\Service\Agenda\EventService;
@@ -123,192 +113,6 @@ class UserController extends AbstractController
     public function styleguide(): Response
     {
         return $this->render('user/pages/styleguide/index.html.twig');
-    }
-
-    /**
-     * @Route("/biens", options={"expose"=true}, name="biens")
-     */
-    public function biens(Request $request, SerializerInterface $serializer, SearchService $searchService): Response
-    {
-        $em = $this->doctrine->getManager();
-
-        $status = $request->query->get('st');
-        $filterNego = $request->query->get('fn');
-        $filterUser = $request->query->get('fu');
-
-        /** @var User $user */
-        $user = $this->getUser();
-        $agencies = $em->getRepository(ImAgency::class)->findBy(['society' => $user->getSociety()]);
-
-        if($status == null){
-            $objs = $em->getRepository(ImBien::class)->findBy(['agency' => $agencies]);
-        }else{
-            $objs = $em->getRepository(ImBien::class)->findBy(['agency' => $agencies, 'status' => (int) $status]);
-        }
-
-        $rapprochements = $searchService->getRapprochementBySearchs($objs, $user->getAgency());
-        $suivis         = $em->getRepository(ImSuivi::class)->findByStatusProcessAndBiens($objs);
-        $contrats       = $em->getRepository(ImContract::class)->findBy(['bien' => $objs, 'status' => ImContract::STATUS_PROCESSING]);
-        $contractants   = $em->getRepository(ImContractant::class)->findBy(['contract' => $contrats]);
-
-        $objs           = $serializer->serialize($objs, 'json', ['groups' => User::USER_READ]);
-        $suivis         = $serializer->serialize($suivis, 'json', ['groups' => ImSuivi::SUIVI_READ]);
-        $contractants   = $serializer->serialize($contractants, 'json', ['groups' => ImContractant::CONTRACTANT_OWNER_READ]);
-
-        return $this->render('user/pages/biens/index.html.twig', [
-            'user' => $user,
-            'data' => $objs,
-            'st' => $status,
-            'filterNego' => $filterNego,
-            'filterUser' => $filterUser,
-            'rapprochements' => json_encode($rapprochements),
-            'suivis' => $suivis,
-            'contractants' => $contractants,
-        ]);
-    }
-
-    /**
-     * @Route("/biens/carte", name="biens_map")
-     */
-    public function carte(Request $request, SerializerInterface $serializer): Response
-    {
-        $em = $this->doctrine->getManager();
-
-        /** @var User $user */
-        $user = $this->getUser();
-        $objs = $em->getRepository(ImBien::class)->findBy(['agency' => $user->getAgency(), 'status' => ImBien::STATUS_ACTIF]);
-
-        $objs = $serializer->serialize($objs, 'json', ['groups' => User::USER_READ]);
-
-        return $this->render('user/pages/biens/map.html.twig', [
-            'user' => $user,
-            'data' => $objs,
-        ]);
-    }
-
-    /**
-     * Common return createBien and updateBien
-     */
-    private function formBien(SerializerInterface $serializer, $route, $obj = null, $element = null, $rooms = "[]", $photos="[]"): Response
-    {
-        $em = $this->doctrine->getManager();
-
-        /** @var User $user */
-        $user = $this->getUser();
-        $negotiators  = $em->getRepository(ImNegotiator::class)->findBy(['agency' => $user->getAgency()]);
-        $allOwners    = $em->getRepository(ImOwner::class)->findBy(['agency' => $user->getAgency()]);
-        $settings     = $em->getRepository(ImSettings::class)->findOneBy(['agency' => $user->getAgency()]);
-        $allSupports  = $em->getRepository(ImSupport::class)->findBy(['agency' => $user->getAgency()]);
-        $publishes    = $em->getRepository(ImPublish::class)->findBy(['bien' => $obj]);
-        $contracts    = $em->getRepository(ImContract::class)->findBy(['bien' => $obj, 'status' => ImContract::STATUS_PROCESSING]);
-        $contractants = $em->getRepository(ImContractant::class)->findBy(['contract' => $contracts]);
-
-        $owners = [];
-        foreach($contractants as $contractant){
-            $owners[] = $contractant->getOwner();
-        }
-
-        $negotiators = $serializer->serialize($negotiators, 'json', ['groups' => User::ADMIN_READ]);
-        $owners      = $serializer->serialize($owners, 'json', ['groups' => User::ADMIN_READ]);
-        $allOwners   = $serializer->serialize($allOwners, 'json', ['groups' => User::ADMIN_READ]);
-        $settings    = $serializer->serialize($settings,'json', ['groups' => User::USER_READ]);
-        $allSupports = $serializer->serialize($allSupports,'json', ['groups' => User::USER_READ]);
-        $publishes   = $serializer->serialize($publishes,'json', ['groups' => ImPublish::PUBLISH_READ]);
-
-        $quartiers = $this->getDonneeData($em, DoQuartier::class, $user, $serializer);
-        $sols      = $this->getDonneeData($em, DoSol::class, $user, $serializer);
-        $sousTypes = $this->getDonneeData($em, DoSousType::class, $user, $serializer);
-
-        return $this->render($route, [
-            'element' => $element,
-            'rooms' => $rooms,
-            'photos' => $photos,
-            'negotiators' => $negotiators,
-            'owners' => $owners,
-            'allOwners' => $allOwners,
-            'settings' => $settings,
-            'user' => $user,
-            'quartiers' => $quartiers,
-            'sols' => $sols,
-            'sousTypes' => $sousTypes,
-            'allSupports' => $allSupports,
-            'publishes' => $publishes,
-        ]);
-    }
-
-    /**
-     * @Route("/biens/bien/ajouter", options={"expose"=true}, name="biens_create")
-     */
-    public function createBien(SerializerInterface $serializer): Response
-    {
-        return $this->formBien($serializer, 'user/pages/biens/create.html.twig');
-    }
-
-    /**
-     * @Route("/biens/bien/modifier/{slug}",options={"expose"=true}, name="biens_update")
-     */
-    public function updateBien(Request $request, $slug, SerializerInterface $serializer): Response
-    {
-        $em = $this->doctrine->getManager();
-
-        $obj     = $em->getRepository(ImBien::class)->findOneBy(["slug" => $slug]);
-        $photos  = $em->getRepository(ImPhoto::class)->findBy(["bien" => $obj]);
-        $rooms   = $em->getRepository(ImRoom::class)->findBy(["bien" => $obj]);
-
-        $element = $serializer->serialize($obj, 'json', ['groups' => User::USER_READ]);
-        $photos  = $serializer->serialize($photos,  'json', ['groups' => User::USER_READ]);
-        $rooms   = $serializer->serialize($rooms,   'json', ['groups' => User::USER_READ]);
-
-        return $this->formBien($serializer, 'user/pages/biens/update.html.twig', $obj, $element, $rooms, $photos);
-    }
-
-    /**
-     * @Route("/biens/bien/suivi/{slug}",options={"expose"=true}, name="biens_suivi")
-     */
-    public function suiviBien(Request $request, $slug, SerializerInterface $serializer, SearchService $searchService): Response
-    {
-        $em = $this->doctrine->getManager();
-        $context     = $request->query->get("ct");
-        $contextRapp = $request->query->get('ctra');
-
-        $obj       = $em->getRepository(ImBien::class)->findOneBy(["slug" => $slug]);
-        $photos    = $em->getRepository(ImPhoto::class)->findBy(["bien" => $obj]);
-        $rooms     = $em->getRepository(ImRoom::class)->findBy(["bien" => $obj]);
-        $visits    = $em->getRepository(ImVisit::class)->findBy(['bien' => $obj]);
-        $suivis    = $em->getRepository(ImSuivi::class)->findBy(['bien' => $obj]);
-        $offers    = $em->getRepository(ImOffer::class)->findBy(['bien' => $obj]);
-        $contracts = $em->getRepository(ImContract::class)->findBy(['bien' => $obj]);
-        $contractants = $em->getRepository(ImContractant::class)->findBy(['contract' => $contracts]);
-
-        $element      = $serializer->serialize($obj,       'json', ['groups' => User::USER_READ]);
-        $photos       = $serializer->serialize($photos,    'json', ['groups' => User::USER_READ]);
-        $rooms        = $serializer->serialize($rooms,     'json', ['groups' => User::USER_READ]);
-        $suivis       = $serializer->serialize($suivis,    'json', ['groups' => ImSuivi::SUIVI_READ]);
-        $visits       = $serializer->serialize($visits,    'json', ['groups' => ImVisit::VISIT_READ]);
-        $offers       = $serializer->serialize($offers,    'json', ['groups' => ImOffer::OFFER_READ]);
-        $contracts    = $serializer->serialize($contracts, 'json', ['groups' => ImContract::CONTRACT_READ]);
-        $contractants = $serializer->serialize($contractants, 'json', ['groups' => ImContractant::CONTRACTANT_OWNER_READ]);
-
-        $historiesVisits = $em->getRepository(HiVisite::class)->findBy(['bienId' => $obj->getId()], ['createdAt' => 'DESC']);
-        $historiesVisits = $serializer->serialize($historiesVisits, 'json', ['groups' => HiVisite::HISTORY_VISITE]);
-        $rapprochements  = $searchService->getRapprochementBySearchs([$obj], $obj->getAgency());
-        $rapprochements  = json_encode($rapprochements);
-
-        return $this->render("user/pages/biens/suivi.html.twig", [
-            'context' => $context,
-            'ctRa' => $contextRapp ?: "tous",
-            'elem' => $obj,
-            'data' => $element,
-            'rooms' => $rooms,
-            'photos' => $photos,
-            'suivis' => $suivis,
-            'visits' => $visits,
-            'offers' => $offers,
-            'contracts' => $contracts,
-            'rapprochements' => $rapprochements,
-            'historiesVisits' => $historiesVisits,
-            'contractants' => $contractants,
-        ]);
     }
 
     /**
@@ -681,47 +485,6 @@ class UserController extends AbstractController
 
         return $this->render('user/pages/settings/supports.html.twig', [
             'donnees' => $data,
-        ]);
-    }
-
-    /**
-     * @Route("/publication", name="publications")
-     */
-    public function publications(SerializerInterface $serializer): Response
-    {
-        $em = $this->doctrine->getManager();
-
-        /** @var User $user */
-        $user = $this->getUser();
-        $data = $em->getRepository(ImBien::class)->findBy([
-            'agency' => $user->getAgency(), 'status' => ImBien::STATUS_ACTIF, 'isDraft' => false, 'isArchived' => false
-        ]);
-        $publishes = $em->getRepository(ImPublish::class)->findBy(['bien' => $data]);
-
-        $data = $serializer->serialize($data, 'json', ['groups' => User::USER_READ]);
-        $publishes = $serializer->serialize($publishes, 'json', ['groups' => ImPublish::PUBLISH_READ]);
-
-        return $this->render('user/pages/publications/index.html.twig', [
-            'donnees' => $data,
-            'publishes' => $publishes,
-        ]);
-    }
-
-    /**
-     * @Route("/publication/historique", name="publications_histories")
-     */
-    public function publicationsHistories(SerializerInterface $serializer): Response
-    {
-        $em = $this->doctrine->getManager();
-
-        /** @var User $user */
-        $user = $this->getUser();
-        $stats = $em->getRepository(ImStat::class)->findBy(['agency' => $user->getAgency()], ['createdAt' => 'DESC']);
-
-        $stats = $serializer->serialize($stats, 'json', ['groups' => ImStat::STAT_READ]);
-
-        return $this->render('user/pages/publications/history.html.twig', [
-            'donnees' => $stats,
         ]);
     }
 
